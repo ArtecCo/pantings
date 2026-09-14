6cab98e2b2138f1ae6ff8baed63d1e5fe91aeabd
Artec.Co <artec.co.online@gmail.com>
2026-09-14 19:02:50 +0530

    Load targeted responsive customer styles

--- customer/src/main.jsx
+++ customer/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './responsive.css'
 import './animations.css'
 import App from './App.jsx'
 import { ToastProvider } from './components/ToastProvider.jsx'
