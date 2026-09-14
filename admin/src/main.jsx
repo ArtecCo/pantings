67b8acf1956171aa4bb83a99371dd8f756efc7be
Artec.Co <artec.co.online@gmail.com>
2026-09-14 20:59:21 +0530

    Initialize painting size editor

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './animations.css'
 import './painting-size-options.css'
+import './painting-size-enhancer.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
