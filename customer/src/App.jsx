205560313ddfa5bc3846afd2efdfa8045c6ab45c
Archith Chinnadamane <126151104+archith1915@users.noreply.github.com>
2026-09-14 14:10:27 +0530

    Centralize customer router and toast provider

--- customer/src/App.jsx
+++ customer/src/App.jsx
@@ -7,24 +7,21 @@ import Account from './pages/Account'
 import Cart from './pages/Cart'
 import TrackOrders from './pages/TrackOrders'
 import Layout from './components/Layout'
-import { ToastProvider } from './components/ToastProvider'
 
 function App() {
   return (
     <BrowserRouter>
-      <ToastProvider>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/paintings" element={<Paintings />} />
-            <Route path="/paintings/:id" element={<PaintingDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/account" element={<Account />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/track-orders" element={<TrackOrders />} />
-          </Routes>
-        </Layout>
-      </ToastProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/paintings" element={<Paintings />} />
+          <Route path="/paintings/:id" element={<PaintingDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/track-orders" element={<TrackOrders />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
