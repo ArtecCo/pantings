46f6a26e537946d708e9b0cf75434221b15353e5
Artec.Co <artec.co.online@gmail.com>
2026-09-15 20:07:26 +0530

    Support PHPMailer without Composer

--- api/mail/Mailer.php
+++ api/mail/Mailer.php
@@ -1,11 +1,16 @@
 <?php
 declare(strict_types=1);
 
-use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 
+$manualBase = __DIR__ . '/../lib/PHPMailer/src/';
 $autoload = __DIR__ . '/../vendor/autoload.php';
-if (is_file($autoload)) {
+
+if (is_file($manualBase . 'Exception.php')) {
+    require_once $manualBase . 'Exception.php';
+    require_once $manualBase . 'PHPMailer.php';
+    require_once $manualBase . 'SMTP.php';
+} elseif (is_file($autoload)) {
     require_once $autoload;
 }
 
@@ -15,24 +20,32 @@ function mailConfig(string $key, string $default = ''): string {
 
 function createMailer(string $fromAddress, string $fromName): PHPMailer {
     if (!class_exists(PHPMailer::class)) {
-        throw new RuntimeException('PHPMailer is not installed. Run composer install in the API directory.');
+        throw new RuntimeException('PHPMailer is not installed. Upload PHPMailer to api/lib/PHPMailer/src/.');
     }
 
     $mail = new PHPMailer(true);
     $mail->isSMTP();
     $mail->Host = mailConfig('MAIL_HOST', '127.0.0.1');
     $mail->Port = (int)mailConfig('MAIL_PORT', '25');
-    $mail->SMTPAuth = mailConfig('MAIL_USERNAME') !== '';
+
+    $username = mailConfig('MAIL_USERNAME');
+    $password = mailConfig('MAIL_PASSWORD');
+    $mail->SMTPAuth = $username !== '';
     if ($mail->SMTPAuth) {
-        $mail->Username = mailConfig('MAIL_USERNAME');
-        $mail->Password = mailConfig('MAIL_PASSWORD');
+        $mail->Username = $username;
+        $mail->Password = $password;
     }
+
     $encryption = strtolower(mailConfig('MAIL_ENCRYPTION'));
     if ($encryption === 'tls') {
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     } elseif ($encryption === 'ssl') {
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    } else {
+        $mail->SMTPSecure = '';
+        $mail->SMTPAutoTLS = false;
     }
+
     $mail->CharSet = 'UTF-8';
     $mail->setFrom($fromAddress, $fromName);
     return $mail;
@@ -45,7 +58,8 @@ function sendHtmlMail(string $fromAddress, string $fromName, string $toAddress, 
         $mail->isHTML(true);
         $mail->Subject = $subject;
         $mail->Body = $html;
-        $mail->AltBody = trim(preg_replace('/<[^>]+>/', ' ', html_entity_decode($html, ENT_QUOTES | ENT_HTML5, 'UTF-8')) ?? '');
+        $plain = preg_replace('/<br\s*\/?\s*>/i', "\n", $html) ?? $html;
+        $mail->AltBody = trim(preg_replace('/<[^>]+>/', ' ', html_entity_decode($plain, ENT_QUOTES | ENT_HTML5, 'UTF-8')) ?? '');
         $mail->send();
         return true;
     } catch (Throwable $e) {
@@ -58,13 +72,9 @@ function sendHtmlMailToMany(string $fromAddress, string $fromName, array $recipi
     $sent = 0;
     foreach ($recipients as $recipient) {
         $email = trim((string)($recipient['email'] ?? ''));
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            continue;
-        }
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) continue;
         $name = trim((string)($recipient['name'] ?? ''));
-        if (sendHtmlMail($fromAddress, $fromName, $email, $name, $subject, $html)) {
-            $sent++;
-        }
+        if (sendHtmlMail($fromAddress, $fromName, $email, $name, $subject, $html)) $sent++;
     }
     return $sent;
 }
