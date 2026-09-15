68151a73c4283a9031c28e6497f67cd038965b74
Artec.Co <artec.co.online@gmail.com>
2026-09-15 19:16:04 +0530

    Use dashboard maintenance control from React

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,7 +5,6 @@ import './animations.css'
 import './painting-size-options.css'
 import './unavailable-paintings.css'
 import './painting-size-enhancer.js'
-import './dashboard-enhancer.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
