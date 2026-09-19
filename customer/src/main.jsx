d211c6b1d379be4dd63559ff4d06ac331ae76cce
Archith Chinnadamane <126151104+archith1915@users.noreply.github.com>
2026-09-20 02:06:38 +0530

    Use heritage toast styling in customer portal

--- customer/src/main.jsx
+++ customer/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './toast.css'
 import './responsive.css'
 import './fixes.css'
 import './collection-redesign.css'
